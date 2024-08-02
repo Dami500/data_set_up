cdc87dab54129d5df20da9600f8d5726f66c4efc
Dami500 <damilareajayiobe0@gmail.com>
2024-08-02 10:06:56 -0400

    Update detect_cointegrated_mean_reversion.py

--- detect_cointegrated_mean_reversion.py
+++ detect_cointegrated_mean_reversion.py
@@ -9,7 +9,7 @@ import warnings
 
 db_host = 'localhost'
 db_user = 'sec_user'
-db_pass = 'Damilare20$'
+db_pass = 'Password'
 db_name = 'securities_master'
 con = msc.connect(host=db_host, user=db_user, password=db_pass, db=db_name)
 
