6b3d4eeaef98758867dbe3fc42d07ba00884fc0d
Dami500 <damilareajayiobe0@gmail.com>
2024-11-16 12:21:54 -0500

    update

--- time_series_tests.py
+++ time_series_tests.py
@@ -105,10 +105,10 @@ def plot_residuals(df):
     plt.plot(df['res'])
     plt.show()
 
-start_date = datetime.datetime(2011,1, 1).strftime('%Y-%m-%d')
-end_date = datetime.datetime(2013, 2, 1).strftime('%Y-%m-%d')
-data = obtain_data_from_sec_master(503,start_date, end_date)
-print(data)
+# start_date = datetime.datetime(2011,1, 1).strftime('%Y-%m-%d')
+# end_date = datetime.datetime(2013, 2, 1).strftime('%Y-%m-%d')
+# data = obtain_data_from_sec_master(503,start_date, end_date)
+# print(data)
 # if __name__ == "__main__":
 #     WMB = obtain_data_from_sec_master('10042')
 #     KMI = obtain_data_from_sec_master('9827')
