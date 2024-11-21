3e116f13829431cc5c8d6cc416ee69eabdab9ca7
Dami500 <damilareajayiobe0@gmail.com>
2024-11-21 18:47:50 -0500

    update

--- price retrieval.py
+++ price retrieval.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import datetime
-from datetime import date
 import warnings
 import mysql.connector as msc
 import requests
@@ -12,16 +11,10 @@ from pandas import Timestamp
 
 db_host = 'localhost'
 db_user = 'sec_user'
-db_pass = 'Password'
+db_pass = 'Damilare20$'
 db_name = 'securities_master'
-try:
-    con = msc.connect(host=db_host, user=db_user, passwd=db_pass, db=db_name, connect_timeout=28800)
-    if con is not None:
-        print("Connected to MySQL database successfully!")
-    else:
-        print("Failed to connect to MySQL database.")
-except msc.Error as e:
-    print(f"Error connecting to MySQL: {e}")
+plug ='caching_sha2_password'
+con = msc.connect(host=db_host, user=db_user, password=db_pass, db=db_name, auth_plugin= plug)
 
 
 def obtain_list_of_db_tickers():
@@ -31,8 +24,8 @@ def obtain_list_of_db_tickers():
     con = msc.connect(host=db_host, user=db_user, passwd=db_pass, db=db_name, connect_timeout=28800)
     with con:
         cur = con.cursor()
-        cur.execute("""SELECT securities_master.‘symbol‘.‘id‘, securities_master.‘symbol‘.‘ticker‘
-        FROM securities_master.‘symbol‘ """)
+        cur.execute("""SELECT securities_master.symbol.id, securities_master.symbol.ticker
+        FROM securities_master.symbol """)
         data = cur.fetchall()
         db_tick = []
         for d in data:
@@ -82,10 +75,10 @@ def convert_to_daily_data(data_vendor_id, symbol_id, data):
 
 
 def update_the_pricing_data(data_vendor_id, symbol_id, data):
-    new = ()
+    now = datetime.datetime.now()
     records = []
     for tup in data:
-        if tup(0) >= Timestamp('2024-07-10 00:00:00-0400', tz='America/New_York'):
+        if tup[0]>= Timestamp('2024-10-29 00:00:00', tz='America/New_York'):
             new = (data_vendor_id, symbol_id, tup[0], now, now, tup[1], tup[2], tup[3], tup[4], tup[5], tup[6])
             records.append(new)
     return records
@@ -99,13 +92,13 @@ def insert_daily_data_into_db(daily_data):
     adj_close and volume)
     """
     # Create the insert strings
-    insert_str = """ INSERT INTO securities_master.‘daily_price‘
-    (securities_master.‘daily_price‘.‘data_vendor_id‘, securities_master.‘daily_price‘.‘symbol_id‘,
-    securities_master.‘daily_price‘.‘price_date‘, securities_master.‘daily_price‘.‘created_date‘,
-    securities_master.‘daily_price‘.‘last_updated_date‘, securities_master.‘daily_price‘.‘open_price‘,
-    securities_master.‘daily_price‘.‘high_price‘, securities_master.‘daily_price‘.‘low_price‘, 
-    securities_master.‘daily_price‘.‘close_price‘, securities_master.‘daily_price‘.‘volume‘, 
-    securities_master.‘daily_price‘.‘adj_close_price‘) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    insert_str = """ INSERT INTO securities_master.daily_price
+    (securities_master.daily_price.data_vendor_id, securities_master.daily_price.symbol_id,
+    securities_master.daily_price.price_date, securities_master.daily_price.created_date,
+    securities_master.daily_price.last_updated_date, securities_master.daily_price.open_price,
+    securities_master.daily_price.high_price, securities_master.daily_price.low_price, 
+    securities_master.daily_price.close_price, securities_master.daily_price.volume, 
+    securities_master.daily_price.split) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
     # Using the MySQL connection, carry out an INSERT INTO for every symbol
     con = msc.connect(host=db_host, user=db_user, passwd=db_pass, db=db_name, connect_timeout=28800)
@@ -115,20 +108,19 @@ def insert_daily_data_into_db(daily_data):
         con.commit()
 
 
-# if __name__ == "__main__":
-#     # This ignores the warnings regarding Data Truncation
-#     # from the Yahoo precision to Decimal(19,4) datatypes
-#     warnings.filterwarnings('ignore')
-#     # Loop over the tickers and insert the daily historical
-#     # data into the database
-#     tickers = obtain_list_of_db_tickers()
-#     len_tickers = len(tickers)
-#     for i, t in enumerate(tickers):
-#         print("Adding data for %s: %s out of %s" % (t[1], i+1, len_tickers))
-#         yf_data = get_daily_historic_data_yahoo(t[1])
-#         dyf_data = convert_numpy_int_to_int(yf_data)
-#         # new_data = convert_to_daily_data('1', t[0], dyf_data)
-#         new_data = update_the_pricing_data('1', t[0], dyf_data)
-#         print(new_data[0])
-#         insert_daily_data_into_db(new_data)
-#     print("Successfully added Yahoo Finance pricing data to DB.")
+if __name__ == "__main__":
+    # This ignores the warnings regarding Data Truncation
+    # from the Yahoo precision to Decimal(19,4) datatypes
+    warnings.filterwarnings('ignore')
+    # Loop over the tickers and insert the daily historical
+    # data into the database
+    tickers = obtain_list_of_db_tickers()
+    len_tickers = len(tickers)
+    for i, t in enumerate(tickers):
+        print("Adding data for %s: %s out of %s" % (t[1], i+1, len_tickers))
+        yf_data = get_daily_historic_data_yahoo(t[1])
+        dyf_data = convert_numpy_int_to_int(yf_data)
+        # new_data = convert_to_daily_data('1', t[0], dyf_data)
+        new_data = update_the_pricing_data('1', t[0], dyf_data)
+        insert_daily_data_into_db(new_data)
+    print("Successfully added Yahoo Finance pricing data to DB.")
